f17151d13735618b68ebe3812167fa3a0a2d19b9
ShaneHsu <xiaosmall8@gmail.com>
2019-06-29 19:23:46 +0800

    加入task

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,20 +9,34 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
             string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
 
             ImageProcess imageProcess = new ImageProcess();
 
-            imageProcess.Clean(destinationPath);
-
             Stopwatch sw = new Stopwatch();
+
             sw.Start();
+            imageProcess.Clean(destinationPath);
+            sw.Stop();
+            Console.WriteLine($"Clean花費時間: {sw.ElapsedMilliseconds} ms");
+
+            sw.Restart();
             imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
             sw.Stop();
 
+            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+
+            sw.Restart();
+            imageProcess.Clean(destinationPath);
+            sw.Stop();
+            Console.WriteLine($"Clean花費時間: {sw.ElapsedMilliseconds} ms");
+
+            sw.Restart();
+            await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+            sw.Stop();
             Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
         }
     }
